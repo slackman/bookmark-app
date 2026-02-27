ee2db1a40c9ffad583c0e6ace53a7758cb0ef9d2
Nikita <nikita@example.com>
2026-02-27 16:45:25 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import ProfileAvatar from './components/ProfileAvatar.vue';
+import { API_ROUTES } from './api';
+import type { Profile } from './interfaces/profile';
+
+const profile = ref<Profile>();
+
+async function fetchProfile() {
+  const data = await fetch(API_ROUTES.profile);
+  const res = (await data.json()) as Profile;
+  profile.value = res;
+}
+
+onMounted(() => {
+  fetchProfile();
+});
+</script>
+
+<template>
+  <div class="app">
+    <nav class="nav">
+      <ProfileAvatar v-if="profile" :name="profile.name" />
+    </nav>
+    <main>Контент</main>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  display: flex;
+  min-height: calc(100vh - 140px);
+  gap: 200px;
+  max-width: 1450px;
+  margin: 140px auto 0 200px;
+}
+
+.nav {
+  min-width: 400px;
+}
+</style>
---
+++ src/components/ProfileAvatar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const { name } = defineProps<{ name: string }>()
+</script>
+
+<template>
+    <div class="profile-avatar">
+        <img src="/public/avatar.png" class="profile-avatar__image" alt="Изображение пользователя" />
+        <div>Привет, <strong>{{ name }}</strong></div>
+    </div>
+</template>
+
+<style scoped>
+.profile-avatar {
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    font-size: 18px;
+}
+
+.profile-avatar__image {
+    width: 80px;
+    height: 80px;
+}
+</style>
